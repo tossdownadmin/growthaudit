6d24305d5cc710fe92542e6ab2ca101c3b6a5467
tossdownadmin <68540790+tossdownadmin@users.noreply.github.com>
2026-09-18 20:15:35 +0500

    fix: defer Google Analytics loading

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -10,9 +10,9 @@ export function GoogleAnalytics({ measurementId }: { measurementId?: string }) {
     <>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${safeMeasurementId}`}
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       />
-      <Script id="ga4-init" strategy="afterInteractive">
+      <Script id="ga4-init" strategy="lazyOnload">
         {`window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
